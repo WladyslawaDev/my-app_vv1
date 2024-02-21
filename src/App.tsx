50f29423649758dd235ba4c1f0ada82da30fe3e5
Lucy <kosmos.vera@gmail.com>
2024-02-21 22:55:59 +0100

    i Typescript  my app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Lesson20 from "./lessons/lesson20/Lesson20";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Lesson20 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/lessons/lesson20/Lesson20.tsx
@@ -0,0 +1,11 @@
+function Lesson20() {
+    let someString: string = "This is string";
+    console.log(someString);
+    someString="New string";
+    console.log(someString);
+
+    let someNumber: number =30;
+    return <div>Lesson 20</div>;
+  }
+  
+  export default Lesson20;
